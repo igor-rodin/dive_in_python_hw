4c536a28250da20fec01e9f8a05d826517029a5b
Игорь Родин <igor-rodin@iMac-Igor.local>
2023-03-13 11:38:04 +0300

    Update task 2

--- task_2.py
+++ task_2.py
@@ -1,42 +1,47 @@
 import math
 
+
+def normalize(nominator: int, denominator: int) -> tuple:
+    gcd_ = math.gcd(nominator, denominator)
+    nominator //= gcd_
+    denominator //= gcd_
+    return (nominator, denominator)
+
+
 fract_1: str = input("Введите первую дробь ('a/b'): ")
 fract_2: str = input("Введите вторую дробь ('a/b'): ")
 
-parts_1 = fract_1.split('/')
-parts_2 = fract_2.split('/')
+nom_denom_1 = fract_1.split('/')
+nom_denom_2 = fract_2.split('/')
 
 # Находим сумму дробей
 
-numenator_sum = int(parts_1[0]) * int(parts_2[1]) + \
-    int(parts_2[0]) * int(parts_1[1])
-denomenator_sum = int(parts_1[1]) * int(parts_2[1])
+nominator_sum = int(nom_denom_1[0]) * int(nom_denom_2[1]) + \
+    int(nom_denom_2[0]) * int(nom_denom_1[1])
+denominator_sum = int(nom_denom_1[1]) * int(nom_denom_2[1])
 
-if numenator_sum % denomenator_sum == 0:
-    sum_res = str(numenator_sum // denomenator_sum)
+if nominator_sum % denominator_sum == 0:
+    sum_res = str(nominator_sum // denominator_sum)
 else:
-    gcd_ = math.gcd(numenator_sum, denomenator_sum)
-    numenator_sum //= gcd_
-    denomenator_sum //= gcd_
-    sum_res = f"{numenator_sum}/{denomenator_sum}"
+    nominator_sum, denominator_sum = normalize(nominator_sum, denominator_sum)
+    sum_res = f"{nominator_sum}/{denominator_sum}"
 
-sum_fr = f"{fract_1} + {fract_2} = {sum_res}"
+sum_fr = f"({fract_1}) + ({fract_2}) = {sum_res}"
 
 
 # Находим произведение дробей
 
-numenator_prod = int(parts_1[0]) * int(parts_2[0])
-denomenator_prod = int(parts_1[1]) * int(parts_2[1])
+nominator_prod = int(nom_denom_1[0]) * int(nom_denom_2[0])
+denominator_prod = int(nom_denom_1[1]) * int(nom_denom_2[1])
 
-if numenator_prod % denomenator_prod == 0:
-    prod_res = str(numenator_prod // denomenator_prod)
+if nominator_prod % denominator_prod == 0:
+    prod_res = str(nominator_prod // denominator_prod)
 else:
-    gcd_ = math.gcd(numenator_prod, denomenator_prod)
-    numenator_prod //= gcd_
-    denomenator_prod //= gcd_
-    prod_res = f"{numenator_prod}/{denomenator_prod}"
+    nominator_prod, denominator_prod = normalize(
+        nominator_prod, denominator_prod)
+    prod_res = f"{nominator_prod}/{denominator_prod}"
 
-prod_fr = f"{fract_1} * {fract_2} = {prod_res}"
+prod_fr = f"({fract_1}) * ({fract_2}) = {prod_res}"
 
 
 print(sum_fr)
