f2caf218395313363349349c4c2c679a4fbd65c1
Игорь Родин <igor-rodin@iMac-Igor.local>
2023-03-17 23:54:23 +0300

    Update task 3 (brute force method)

--- task_3.py
+++ task_3.py
@@ -1,21 +1,26 @@
-KNAPSACK_CAPACITY = 13
+KNAPSACK_CAPACITY = 10
 
 goods = {'item_1': 9, 'item_2': 1, 'item_3': 5,
          'item_4': 3, 'item_5': 10, 'item_6': 7, 'item_7': 2, 'item_8': 15}
-goods = filter(lambda item: item[1] <= KNAPSACK_CAPACITY, goods.items())
-goods = dict(sorted(goods, key=lambda item: item[1]))
+goods = dict(filter(lambda item: item[1] <= KNAPSACK_CAPACITY, goods.items()))
 
 knapsack_weight = 0
 goods_in_knapsack = []
 
 goods_list = list(goods.items())
-for i in range(len(goods)):
-    can_take = []
-    for (item, weight) in goods_list[i:]:
-        if knapsack_weight + weight <= KNAPSACK_CAPACITY:
+
+# Brute force (полным перебором)
+
+for i in range(1, 2 ** len(goods_list)):
+    powerTwo = 1
+    can_get = []
+    for item, weight in goods_list:
+        if powerTwo & i != 0:
             knapsack_weight += weight
-            can_take.append((item, weight))
-    goods_in_knapsack.append(can_take)
+            can_get.append((item, weight))
+        powerTwo *= 2
+    if knapsack_weight <= KNAPSACK_CAPACITY:
+        goods_in_knapsack.append(can_get)
     knapsack_weight = 0
 
 for i, val in enumerate(goods_in_knapsack, start=1):
