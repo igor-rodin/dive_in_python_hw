b08200f7307b0706fff5e6cb447b4c2364dc5df7
Игорь Родин <igor-rodin@iMac-Igor.local>
2023-03-18 02:53:40 +0300

    Done task 1

---
+++ task_1.py
@@ -0,0 +1,23 @@
+import random
+N: int = 3
+M: int = 4
+
+matrix = [[random.randrange(10) for _ in range(1, N + 1)]
+          for _ in range(1, M + 1)]
+
+
+def print_matrix(matrix: list[list[int | float]]) -> None:
+    for row in matrix:
+        print(f"{row}", end="\t")
+        print()
+
+
+def transpose(matrix: list[list[int | float]]) -> list[list[int | float]]:
+    return [[matrix[col][row] for col in range(len(matrix))] for row in range(len(matrix[0]))]
+
+
+print("Исходная матрица")
+print_matrix(matrix)
+transp_matrix = transpose(matrix)
+print("Транспонированная матрица")
+print_matrix(transp_matrix)
