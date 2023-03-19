b08f78c97923a3d0bf2c168c5d00a83312d8163a
Игорь Родин <igor-rodin@iMac-Igor.local>
2023-03-19 18:06:28 +0300

    Update task 1

--- task_1.py
+++ task_1.py
@@ -13,7 +13,10 @@ def print_matrix(matrix: list[list[int | float]]) -> None:
 
 
 def transpose(matrix: list[list[int | float]]) -> list[list[int | float]]:
-    return [[matrix[col][row] for col in range(len(matrix))] for row in range(len(matrix[0]))]
+    '''
+    Возвращает трансонированную матрицу
+    '''
+    return [[matrix[row][col] for row in range(len(matrix))] for col in range(len(matrix[0]))]
 
 
 print("Исходная матрица")
