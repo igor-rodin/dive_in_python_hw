0defc788ed2b0e8d0ac2922c3e3a1f7e837ce0e9
igor-rodin <igreen1974@gmail.com>
2023-03-26 16:57:37 +0300

    Add gen_queens_position function

--- lesson_6/chess.py
+++ lesson_6/chess.py
@@ -1,8 +1,27 @@
+import random
+
 '''
 Задача о 8 ферзях
 '''
 
-__all__ = ['check_bit_queen']
+__all__ = ['check_bit_queen', 'gen_queens_position']
+
+
+def gen_queens_position(queen_count: int) -> tuple[(int, int)]:
+    '''
+    Генерирует случаюную расстановку queen_count ферзей на доске
+    '''
+    pos = []
+    prev_row = None
+    for _ in range(queen_count):
+        cur_row = random.randrange(1, queen_count + 1)
+        while cur_row == prev_row:
+            cur_row = random.randrange(1, queen_count + 1)
+        prev_row = cur_row
+        cur_col = random.randrange(1, queen_count + 1)
+        pos.append((cur_row, cur_col))
+
+    return tuple(pos)
 
 
 def check_bit_queen(positions: tuple((int, int))) -> bool:
@@ -17,8 +36,7 @@ def check_bit_queen(positions: tuple((int, int))) -> bool:
         Значит они бьют друг друга
         '''
         for queen_count in line.values():
-            if queen_count - 1 > 0:
-                print("Two or more queens on one line")
+            if queen_count > 1:
                 return False
         return True
 
