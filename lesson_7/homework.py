7c0a3521649f17baa477b7b56aeb36c2f84535c0
igor-rodin <igreen1974@gmail.com>
2023-03-30 12:04:51 +0300

    Update group_rename function

--- lesson_7/homework.py
+++ lesson_7/homework.py
@@ -5,20 +5,22 @@ from pathlib import Path
 __all__ = ['group_rename']
 
 
-def __rename_file(file: Path, target_ext: str, pref_name: str, source_slice: tuple[int, int],
-                  counter: int, counter_digits: int) -> None:
+def __rename_file(file: Path, target_ext: str, range: tuple[int, int],
+                  counter: int, counter_digits: int, pref_name: str = '') -> None:
     '''
     Переимеменовывает файл
     '''
     *_, file_name_with_ext = str(file).split('/')
-    prev_name = file_name_with_ext[:file_name_with_ext.rfind('.')]
-    new_name = f'{prev_name[source_slice[0]: source_slice[1]]}_{pref_name}_{counter:0>{counter_digits}}.{target_ext}'
+    orig_name = file_name_with_ext[:file_name_with_ext.rfind('.')]
+    if pref_name:
+        pref_name = f'_{pref_name}'
+    new_name = f'{orig_name[range[0]: range[1]]}{pref_name}_{counter:0>{counter_digits}}.{target_ext}'
     old = Path(file_name_with_ext)
     old.replace(Path.cwd() / new_name)
 
 
-def group_rename(source_ext: str, target_ext: str, pref_name: str, source_slice: tuple[int, int],
-                 counter_digits: int, source_dir: str = '') -> None:
+def group_rename(source_ext: str, target_ext: str, range: tuple[int, int],
+                 counter_digits: int, source_dir: str = '', pref_name: str = '') -> None:
     '''
     Переименовывает файлы c расширением source_ext находящиеся в директории source_dir.
     source_dir - абсолютный путь, target_ext - новое расширение
@@ -31,12 +33,12 @@ def group_rename(source_ext: str, target_ext: str, pref_name: str, source_slice:
         _, ext = os.path.splitext(file)
         if ext[1:] == source_ext:
             counter += 1
-            __rename_file(file, target_ext, pref_name,
-                          source_slice, counter, counter_digits)
+            __rename_file(file, target_ext, range,
+                          counter, counter_digits, pref_name)
     if source_dir != cur_dir:
         os.chdir(cur_dir)
 
 
 if __name__ == '__main__':
     dir = Path.cwd() / 'tmp'
-    group_rename('dat', 'dt', 'new', (3, 5), 3, dir)
+    group_rename('dt', 'dat', (0, 4), 2, dir, pref_name='current')
--- main.py
+++ main.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     sem.gen_files_to_dir({'txt': 2, 'xml': 4, 'jpg': 2,
                          'ogg': 3, 'wav': 2, 'bmp': 4}, target_dir=dir)
     sem.group_files_by_ext(dir)
-    hw.group_rename('dt', 'dat', 'old', (3, 5), 2, dir)
+    hw.group_rename('xmlx', 'xml', (0, 5), 2, dir, pref_name='new')
