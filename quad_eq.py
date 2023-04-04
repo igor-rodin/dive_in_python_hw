b8e1bd63bec799860684ac96d85f2e8653a653db
Igor Rodin <igreen1974@gmail.com>
2023-04-05 02:28:11 +0300

    Done gen params to csv file function

--- quad_eq.py
+++ quad_eq.py
@@ -1,8 +1,10 @@
 '''Функции для решения квадратного уравнения'''
 
-__all__ = ['solve_quad']
+__all__ = ['solve_quad', 'gen_params_csv']
 
+from random import uniform
 from typing import Any
+import serialize.s_csv as s_csv
 
 
 def _desc(a: float | int, b: float | int, c: float | int) -> float:
@@ -15,6 +17,21 @@ def _valid(a: Any) -> bool:
     Проверяет тип аргумента уравнения
     '''
     return type(a) == int or type(a) == float
+
+
+def gen_params_csv(file_csv: str = 'params.csv', count=10, min_val=-5, max_val=5) -> None:
+    '''
+    Генерирует параметры квадратного уравнения и записывает в csv файл
+    '''
+    data = []
+    for _ in range(count):
+        params = {}
+        params['a'] = uniform(min_val, max_val)
+        params['b'] = uniform(min_val, max_val)
+        params['c'] = uniform(min_val, max_val)
+        data.append(params)
+
+    s_csv.write_ld_csv(data, file_csv)
 
 
 def solve_quad(* args) -> tuple[float | complex, float | complex] | float | None:
@@ -49,7 +66,7 @@ def solve_quad(* args) -> tuple[float | complex, float | complex] | float | None
 
 if __name__ == '__main__':
     params = [1., 0., -1]
-
+    gen_params_csv()
     roots = solve_quad(*params)
 
     print(f'{roots= }')
