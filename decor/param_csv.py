b11e5260b97574dc7c7db4b5d2ee6cefb6a0f656
Igor Rodin <igreen1974@gmail.com>
2023-04-05 12:35:35 +0300

    Done param_csv decorator

---
+++ decor/param_csv.py
@@ -0,0 +1,21 @@
+'''Содержит декоратор, применяющий данные из csv файла в качестве параметров функции'''
+
+from typing import Callable, Any
+from serialize.s_csv import read_l_csv
+
+CSV_FILE = 'params.csv'
+
+
+def csv_params(csv_file: str = CSV_FILE) -> Callable:
+    def decor(func: Callable) -> Callable:
+        params = []
+        result = []
+        params = read_l_csv(csv_file)
+
+        def wrapper() -> Any:
+            for param in params:
+                res = func(*param)
+                result.append(res)
+            print(f'{result=}')
+        return wrapper
+    return decor
