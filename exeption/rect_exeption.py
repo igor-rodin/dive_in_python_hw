ef8758246170eea9fe7e152b37798f1eca6a89e7
Igor Rodin <igreen1974@gmail.com>
2023-04-22 20:39:11 +0300

    Add RectangleExeptions

---
+++ exeption/rect_exeption.py
@@ -0,0 +1,8 @@
+class NegativeRectSizeExeption(Exception):
+    """Исключение выбрасывается при попвтке создать прямоугольник с отрицательными размерами"""
+
+    def __init__(self, size: int) -> None:
+        self._size = size
+
+    def __str__(self) -> str:
+        return f"Нельзя создать прямоугольник с отрицательной сотороной {self._size}"
