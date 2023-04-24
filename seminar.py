e3acb5264425a02552da9fa8851b7a3145b8ed7d
Igor Rodin <igreen1974@gmail.com>
2023-04-24 13:14:52 +0300

    Done tasks 4

--- seminar.py
+++ seminar.py
@@ -1,6 +1,8 @@
 import string
 import unittest
 
+# import pytest
+
 # Задание №1
 # Создайте функцию, которая удаляет из текста все символы кроме букв латинского алфавита и пробелов.
 # Возвращается строка в нижнем регистре.
@@ -63,10 +65,46 @@ class TestCleanStr(unittest.TestCase):
         self.assertEqual(clean_str("Пример -  Binding Class!!!"), "binding class")
 
 
+# Задание №4
+# Напишите для задачи 1 тесты pytest. Проверьте следующие варианты:
+# - возврат строки без изменений
+# - возврат строки с преобразованием регистра без потери символов
+# - возврат строки с удалением знаков пунктуации
+# - возврат строки с удалением букв других алфавитов
+# - возврат строки с учётом всех вышеперечисленных пунктов (кроме п. 1)
+
+
+def test_str_the_same():
+    assert clean_str("binding class") == "binding class", "Что-то не так"
+
+
+def test_to_lower():
+    assert clean_str("Binding Class") == "binding class", "Что-то не так"
+
+
+def test_del_punctuations():
+    assert clean_str("binding, class!!!") == "binding class", "Что-то не так"
+
+
+def test_del_not_ascii():
+    assert clean_str("пример binding class") == "binding class", "Что-то не так"
+
+
+def test_all():
+    assert clean_str("Пример -  Binding Class!!!") == "binding class", "Что-то не так"
+
+
+# Задание №5
+# На семинарах по ООП был создан класс прямоугольник хранящий длину и ширину, а также вычисляющую периметр,
+# площадь и позволяющий складывать и вычитать прямоугольники беря за основу периметр.
+# Напишите 3-7 тестов unittest для данного класса.
+
 if __name__ == "__main__":
     # text = "Создайте функцию, которая To set up an instaаорпаnce of the binding class for use with a fragmentудаляет из текста все символы кроме букв латинского алфавита и пробелов."
     # print(clean_str(text))
     import doctest
+    import pytest
 
     # doctest.testmod(verbose=True)
-    unittest.main(verbosity=2)
+    # unittest.main(verbosity=2)
+    pytest.main(["-v", "seminar.py"])
